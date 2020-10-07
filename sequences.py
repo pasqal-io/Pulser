58232b8cf1aa779e5be334b1784160cc50092f16
HSilver <henrique.silverio@tecnico.ulisboa.pt>
2020-10-07 17:22:08 +0200

    First major Sequence restructuring

--- sequences.py
+++ sequences.py
@@ -1,77 +1,132 @@
-from abc import ABC, abstractmethod
+import warnings
+from collections import namedtuple
 
-import numpy as np
-import pulses
+from devices import PasqalDevice
+from utils import validate_duration
 
-class Sequence(ABC):
+# Auxiliary class to store the information in the schedule
+TimeSlot = namedtuple('TimeSlot', ['type', 'ti', 'tf', 'targets'])
+
+
+class Sequence:
+    """A sequence of operations on a device.
+
+    A sequence is composed by
+        - The device in which we want to implement it
+        - The device's channels that are used
+        - The schedule of operations on each channel
     """
-    Creates a pulse sequence. A sequence is composed by
-    - The device in which we want to implement this sequence
-    - The parameters of each of the composing pulses
-    - The current queue of pulses to which we can add , modify or delete
-    """
-    def __init__(self, device, parameters, initial_state):
-        self.device = device
-        self.parameters = parameters
-        self.initial_state = initial_state
-        self.queue = []
-        # Maybe add an 'empty' pulse by default
+    def __init__(self, device):
+        if not isinstance(device, PasqalDevice):
+            raise TypeError("The Sequence's device has to be a PasqalDevice.")
+        self._device = device
+        self._channels = {}
+        self._schedule = {}
+        self._taken_channels = []   # Stores the ids of selected channels
+        self._qids = set(self.qubit_info.keys())  # IDs of all qubits in device
 
+    @property
+    def qubit_info(self):
+        """Returns the dictionary with the qubit's IDs and positions."""
+        return self._device.qubits
 
-    def add(self,Pulse):
-        self.queue.append(Pulse)
-        # Check if burst is OK
-        #print(f'ADDED BURST. SEQUENCE IS: {self.queue} ')
+    @property
+    def declared_channels(self):
+        return dict(self._channels)
 
-    @abstractmethod
-    def add_layers(self, Parameters):
+    @property
+    def available_channels(self):
+        return {id: ch for id, ch in self._device.channels.items()
+                if id not in self._taken_channels}
+
+    def declare_channel(self, name, channel_id, initial_target=None):
+        """Declare a new channel to the Sequence.
+
+        Args:
+            name (str): Unique name for the channel in the sequence.
+            channel_id (str): How the channel is identified in the device.
+
+        Keyword Args:
+            initial_target (set, default=None): For 'local' adressing channels
+                where a target has to be defined, it can be done when the
+                channel is first declared. If left as None, this will have to
+                be done manually as the first addition to a channel.
+        """
+
+        if name in self._channels:
+            raise ValueError("The given name is already in use.")
+
+        if channel_id not in self._device.channels:
+            raise ValueError("No channel %s in the device." % channel_id)
+
+        if channel_id in self._taken_channels:
+            raise ValueError("Channel %s has already been added." % channel_id)
+
+        ch = self._device.channels[channel_id]
+        self._channels[name] = ch
+        self._taken_channels.append(channel_id)
+        self._schedule[name] = []
+
+        if ch.addressing == 'global':
+            self._schedule[name].append(TimeSlot('target', -1, 0, self._qids))
+        elif initial_target is not None:
+            self.target(initial_target, name)
+
+    def add(self, pulse, channel, protocol='min-delay'):
+        # TODO
         pass
 
-class CustomSequence(Sequence):
-    """
-    Creates a custom pulse sequence
-    """
-    def __init__(self, device, atom_array, initial_state):
-        super().__init__(device, initial_state)
+    def target(self, qubits, channel):
+        """Changes the target qubit of a 'local' channel.
 
-    # Additional methods
+        Args:
+            qubits (set(str)): The new target for this channel.
+            channel (str): The channel's name.
+        """
 
-class ParamSequence(Sequence):
-    """
-    Creates a predetermined pulse sequence for variational programs with several layers.
-    Recall that parameters can hold information about the type of function that
-    represents rabi or detuning, and whose defining parameters are being optimized
-    """
-    def __init__(self, device, param_set, initial_state='ALL_DOWN'):
-        super().__init__(device, param_set, initial_state)
-        self.depth = len(param_set["intervals"])
-        print("SEQUENCE ACTIVATED")
-        self.add_layers()
-        print(f"BEGIN QAOA. INITIAL STATE IS {initial_state}")
+        if channel not in self._channels:
+            raise ValueError("Use the name of a declared channel.")
 
-    def add_layers(self):
-        # Try a simple sequence of square pulses
-        layer_params = self.get_layer_params(0)
-        pulse = pulses.IsingPulse(layer_params)
-        self.add(pulse)
-        for i in range(1,self.depth):
-            layer_params = self.get_layer_params(i)
-            pulse(layer_params)
-            self.add(pulse)
+        qs = {qubits}
+        if not qs.issubset(self._qids):
+            raise ValueError("The given qubits have to belong to the device.")
 
-    def get_layer_params(self, pos):
-        layer_params = {
-                        "rabi" : self.parameters["rabi"][pos],
-                        "detuning" : self.parameters["detuning"][pos],
-                        "intervals" : self.parameters["intervals"][pos]
-                        }
-        return layer_params
+        if self._channels[channel].addressing != 'local':
+            raise ValueError("Can only choose target of 'local' channels.")
+        elif len(qs) != 1:
+            raise ValueError("This channel takes only a single target qubit.")
 
-    def get_parameter_set(self):
-        ans = []
-        for key,values in self.parameters.items():
-            #print(f"{key}:")
-            ans.append(values)
-        return ans
+        try:
+            last = self._last(channel)
+            if last.targets == qs:
+                warnings.warn("The provided qubits are already the target. "
+                              "Skipping this target instruction.")
+                return
+            ti = last.tf
+            tf = ti + self._channels[channel].retarget_time
+        except ValueError:
+            ti = -1
+            tf = 0
 
-    # There should be a function to modify the existing pulse
+        self._schedule[channel].append(TimeSlot('target', ti, tf, qs))
+
+    def delay(self, duration, channel):
+        """Idle a given choosen for a specific duration.
+
+        Args:
+            duration (int): Time to delay (in ns).
+            channel (str): The channel's name.
+        """
+        last = self._last(channel)
+        ti = last.tf
+        tf = ti + validate_duration(duration)
+        self._schedule[channel].append(TimeSlot('delay', ti, tf, last.targets))
+
+    def _last(self, channel):
+        """Shortcut to last element in the channel's schedule."""
+        if channel not in self._schedule:
+            raise ValueError("Use the name of a declared channel.")
+        try:
+            return self._schedule[channel][-1]
+        except IndexError:
+            raise ValueError("The chosen channel has no target.")
