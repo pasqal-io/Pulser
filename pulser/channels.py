95513c62d478b9b8e1ff4d6e1cb1fe3fb0b5d7e2
HSilveri <henrique.silverio@tecnico.ulisboa.pt>
2020-12-15 12:32:35 +0100

    Update tests

--- pulser/channels.py
+++ pulser/channels.py
@@ -28,33 +28,6 @@ class Channel:
     retarget_time: int = None
     max_targets: int = 1
 
-    # def __init__(self, addressing, max_abs_detuning, max_amp,
-    #              retarget_time=None, max_targets=1):
-    #     """Initializes a channel with specific characteristics."""
-    #     if addressing == 'Local':
-    #         if retarget_time is None:
-    #             raise ValueError("Must set retarget time for local channel.")
-    #         self.retarget_time = int(retarget_time)
-    #         if not isinstance(max_targets, int):
-    #             raise TypeError("max_targets must be an int.")
-    #         elif max_targets < 1:
-    #             raise ValueError("max_targets must be at least 1")
-    #         else:
-    #             self.max_targets = max_targets
-    #
-    #     elif addressing != 'Global':
-    #         raise ValueError("Addressing can only be 'Global' or 'Local'.")
-    #
-    #     self.addressing = addressing
-    #
-    #     if max_abs_detuning < 0:
-    #         raise ValueError("Maximum absolute detuning has to be positive.")
-    #     self.max_abs_detuning = max_abs_detuning
-    #
-    #     if max_amp <= 0:
-    #         raise ValueError("Maximum channel amplitude has to be positive.")
-    #     self.max_amp = max_amp
-
     @classmethod
     def Local(cls, max_abs_detuning, max_amp, retarget_time, max_targets=1):
         """Initializes the channel with local adressing.
--- pulser/tests/test_channels.py
+++ pulser/tests/test_channels.py
@@ -12,33 +12,27 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import pytest
-
+import pulser
 from pulser.channels import Raman, Rydberg
 
 
-def test_init():
-    with pytest.raises(ValueError, match="set retarget time for local"):
-        Raman('Local', 50, 10)
-
-    with pytest.raises(ValueError, match="'Global' or 'Local'"):
-        Raman('total', 20, 1)
-        Rydberg(2, 1, 0)
-
-    with pytest.raises(ValueError, match="detuning has to be positive"):
-        Raman.Global(-1, 1)
-
-    with pytest.raises(ValueError, match="amplitude has to be positive"):
-        Rydberg.Global(1, 0)
-
-    with pytest.raises(TypeError):
-        Raman.Local(1, 19, 100, max_targets=1.5)
-
-    with pytest.raises(ValueError, match='at least 1'):
-        Raman.Local(1, 19, 100, max_targets=0)
-
-    Raman.Local(10, 2, retarget_time=1000)
-    Rydberg.Global(50, 2.5)
+def test_device_channels():
+    for dev in pulser.devices._valid_devices:
+        for i, (id, ch) in enumerate(dev.channels.items()):
+            assert id == dev._channels[i][0]
+            assert isinstance(id, str)
+            assert ch == dev._channels[i][1]
+            assert isinstance(ch, pulser.channels.Channel)
+            assert ch.name in ['Rydberg', 'Raman']
+            assert ch.basis in ['digital', 'ground-rydberg']
+            assert ch.addressing in ['Local', 'Global']
+            assert ch.max_abs_detuning >= 0
+            assert ch.max_amp > 0
+            if ch.addressing == 'Local':
+                assert ch.retarget_time >= 0
+                assert ch.retarget_time == int(ch.retarget_time)
+                assert ch.max_targets >= 1
+                assert ch.max_targets == int(ch.max_targets)
 
 
 def test_repr():
