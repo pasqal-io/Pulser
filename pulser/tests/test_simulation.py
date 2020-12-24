9d5faf3972bbc804684d82a6f61686d17d79d64e
sebgrijalva <sebastian@pasqal.io>
2020-12-24 11:39:31 +0100

    Expand test of `__init__()`.

--- pulser/tests/test_simulation.py
+++ pulser/tests/test_simulation.py
@@ -12,23 +12,80 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from unittest.mock import patch
+#from unittest.mock import patch
 
 import numpy as np
 import pytest
 
+import qutip
+
 from pulser import Sequence, Pulse, Register
-from pulser.devices import Chadoq2, MockDevice
-from pulser.devices._pasqal_device import PasqalDevice
-from pulser.sequence import TimeSlot
+from pulser.devices import Chadoq2
 from pulser.waveforms import BlackmanWaveform
 from pulser.simulation import Simulation
 
-reg = Register.triangular_lattice(4, 7, spacing=5, prefix='q')
+q_dict = {"control1": np.array([-4., 0.]),
+          "target": np.array([0., 4.]),
+          "control2": np.array([4., 0.])}
+reg = Register(q_dict)
 device = Chadoq2
+
+duration = 3000
+pi = Pulse.ConstantDetuning(BlackmanWaveform(duration, np.pi), 0., 0)
+twopi = Pulse.ConstantDetuning(BlackmanWaveform(duration, 2*np.pi), 0., 0)
+pi_Y = Pulse.ConstantDetuning(BlackmanWaveform(duration, np.pi), 0., -np.pi/2)
+
+seq = Sequence(reg, Chadoq2)
+
+# Declare Channels
+seq.declare_channel('rydA', 'rydberg_local', 'control1')
+seq.declare_channel('rydB', 'rydberg_local2', 'control2')
+seq.declare_channel('raman', 'raman_local', 'control1')
+
+d = 0  # Pulse Duration
+t = 0  # Retarget time raman
+
+# Prepare state 'hhh':
+seq.add(pi_Y, 'raman')
+d += 1
+seq.target('target', 'raman')
+t += 1
+seq.add(pi_Y, 'raman')
+d += 1
+seq.target('control2', 'raman')
+t += 1
+seq.add(pi_Y, 'raman')
+d += 1
+
+prep_state = qutip.tensor([qutip.basis(3, 2) for _ in range(3)])
+
+# Write CCZ sequence:
+seq.add(pi, 'rydA', 'wait-for-all')  # Wait for state preparation to finish.
+d += 1
+seq.align('rydA', 'rydB')
+seq.add(pi, 'rydB')
+d += 1
+seq.target('target', 'rydA')
+seq.align('rydA', 'rydB')
+seq.add(twopi, 'rydA')
+d += 1
+seq.align('rydA', 'rydB')
+seq.add(pi, 'rydB')
+d += 1
+seq.target('control1', 'rydA')
+seq.align('rydA', 'rydB')
+seq.add(pi, 'rydA')
+d += 1
 
 
 def test_init():
     fake_sequence = {'pulse1': 'fake', 'pulse2': "fake"}
     with pytest.raises(TypeError, match='sequence has to be a valid'):
         Simulation(fake_sequence)
+    sim = Simulation(seq)
+    assert sim._seq == seq
+    assert sim._qdict == seq.qubit_info
+    assert sim._size == len(seq.qubit_info)
+    assert sim._tot_duration == (duration * d
+                                 + seq._channels['raman'].retarget_time * t)
+    assert sim._qid_index == {"control1": 0, "target": 1, "control2": 2}
