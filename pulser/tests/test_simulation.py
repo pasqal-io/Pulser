0742e16c59092350cf389047c9e58c8de2791bae
Louis-PaulHenry <79902647+Louis-PaulHenry@users.noreply.github.com>
2021-04-09 15:20:31 +0200

    style

--- pulser/tests/test_simulation.py
+++ pulser/tests/test_simulation.py
@@ -228,9 +228,6 @@ def test_single_atom_simulation():
 
 def test_run():
     sim = Simulation(seq, sampling_rate=0.01)
-
-    print((1000*sim._times).astype(int))
-
     with patch('matplotlib.pyplot.show'):
         sim.draw()
     bad_initial = np.array([1.])
