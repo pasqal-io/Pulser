907934988be4c990be54e54e263d54f433c85cf9
HGSilveri <henrique.silverio@tecnico.ulisboa.pt>
2022-02-04 17:15:33 +0100

    WIP: RegisterLayout definition

--- pulser/register/register_layout.py
+++ pulser/register/register_layout.py
@@ -15,7 +15,68 @@
 from __future__ import annotations
 
 from abc import ABC
+from collections.abc import Sequence as abcSequence
+from typing import Optional
+
+import numpy as np
+from numpy.typing import ArrayLike
+
+from pulser.base_register import QubitId, BaseRegister
+from pulser.register.register import Register
+from pulser.register.regiter3d import Register3D
 
 
 class RegisterLayout(ABC):
-    pass
+    def __init__(self, trap_coordinates: ArrayLike):
+        coords = np.array(trap_coordinates, dtype=float)
+        shape = coords.shape
+        if len(shape) != 2:
+            raise ValueError(
+                "'trap_coordinates' must be an array or list of coordinates."
+            )
+        if shape[1] not in (2, 3):
+            raise ValueError(
+                f"Each coordinate must be of size 2 or 3, not {shape[1]}."
+            )
+        self._dim = shape[1]
+        # Sorting the coordinates 1st left to right, 2nd bottom to top
+        rounded_coords = np.round(coords, decimals=6)
+        sorting = np.lexsort((rounded_coords[:, 1], rounded_coords[:, 0]))
+
+        self._coords = coords[sorting]
+
+    @property
+    def traps_dict(self) -> dict:
+        return dict(enumerate(self._coords))
+
+    @property
+    def number_of_traps(self) -> int:
+        return len(self._coords)
+
+    @property
+    def max_atom_num(self) -> int:
+        return self.number_of_traps // 2
+
+    @property
+    def dimensionality(self) -> int:
+        return self._dim
+
+    def define_register(
+        self, *trap_ids: int, qubit_ids: Optional[abcSequence[QubitId]] = None
+    ) -> BaseRegister:
+
+        if len(trap_ids) > self.max_atom_num:
+            raise ValueError(
+                "The number of required traps is greater than the maximum "
+                "number of qubits allowed for this layout "
+                f"({self.max_atom_num})."
+            )
+        ids = (
+            qubit_ids if qubit_ids else [f"q{i}" for i in range(len(trap_ids))]
+        )
+        coords = self._coords[list(trap_ids)]
+        qubits = dict(zip(ids, coords))
+
+        reg_class = Register3D if self._dim == 3 else Register
+
+        return reg_class(qubits)
