fff1e50cb9affe0dbe81edf8c44beaffef536791
Louis Vignoli <louis.vignoli@pasqal.com>
2022-02-04 16:38:43 +0100

    Change sphinx theme to furo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -63,7 +63,8 @@ autodoc_typehints = "none"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+# html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
