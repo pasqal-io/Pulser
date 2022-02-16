0139570d7f96442627f3f176284e4ed1ec946e0c
Louis Vignoli <louis.vignoli@pasqal.com>
2022-02-16 14:09:33 +0100

    Change sphinx theme to book
    Poll results:
    3 | books
    2 | MagnificientGreen
    1 | furo
    1 | press
    0 | ReadTheDocs
    
    Two other contributions steering towards `books`.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -64,7 +64,7 @@ autodoc_typehints = "none"
 # a list of builtin themes.
 #
 # html_theme = "sphinx_rtd_theme"
-html_theme = "furo"
+html_theme = "sphinx_book_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
