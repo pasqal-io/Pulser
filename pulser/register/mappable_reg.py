d52ba52fbc13837aa2eaa27bd72f98e2f49d18f1
CdeTerra <102144942+CdeTerra@users.noreply.github.com>
2022-04-07 11:59:13 +0200

    Add function to convert atoms ids to indices

--- pulser/register/mappable_reg.py
+++ pulser/register/mappable_reg.py
@@ -22,7 +22,7 @@ from pulser.json.utils import obj_to_dict
 if TYPE_CHECKING:  # pragma: no cover
     from pulser.register.base_register import BaseRegister, QubitId
     from pulser.register.register_layout import RegisterLayout
-
+from typing import Sequence as abcSequence
 
 class MappableRegister:
     """A register with the traps of each qubit still to be defined.
@@ -65,11 +65,38 @@ class MappableRegister:
         chosen_ids = tuple(qubits.keys())
         if not set(chosen_ids) <= set(self._qubit_ids):
             raise ValueError(
-                "All qubits must be labeled with pre-declared qubit IDs."
+                f"All qubits must be labeled with pre-declared qubit IDs. {chosen_ids!r}, {self._qubit_ids!r}"
             )
         return self._layout.define_register(
             *tuple(qubits.values()), qubit_ids=chosen_ids
         )
+
+    def find_indices(self, chosen_ids: set[QubitId], id_list: abcSequence[QubitId]) -> list[int]:
+        """
+        Computes indices for the given qubit IDs for a given register mapping.
+
+        This can especially be useful when building a Pulser Sequence with a parameter denoting qubits.
+        Example:
+            ``
+            mapp_reg = TriangularLatticeLayout(50, 5).make_mappable_register(5)
+            qubit_map = {"q0": 1, "q2": 4, "q4": 2}
+            indices = mapp_reg.find_indices(qubit_map, ["q4", "q2", "q1", "q2"])
+            seq.build(qubits=qubit_map, qubit_indices=indices)
+            ``
+
+        Args:
+            chosen_ids: IDs of the qubits that are chosen to map the MappableRegister
+            id_list: IDs of the qubits to denote
+
+        Returns:
+            Indices of the qubits to denote, only valid for the given mapping.
+        """
+        if not chosen_ids <= set(self._qubit_ids):
+            raise ValueError("Chosen IDs must be selected among pre-declared qubit IDs.")
+        if not set(id_list) <= chosen_ids:
+            raise ValueError("The IDs list must be selected among the chosen IDs.")
+        ordered_ids = [id for id in self.qubit_ids if id in chosen_ids]
+        return [ordered_ids.index(id) for id in id_list]
 
     def _to_dict(self) -> dict[str, Any]:
         return obj_to_dict(self, self._layout, *self._qubit_ids)
