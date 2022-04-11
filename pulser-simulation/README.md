b345bf599b66af9b96e1e0b1665c80f2f1f6de1b
HGSilveri <henrique.silverio@tecnico.ulisboa.pt>
2022-04-11 15:48:41 +0200

    Setting up `pulser_simulation` as  a separate package

---
+++ pulser-simulation/README.md
@@ -0,0 +1,21 @@
+# Pulser-Simulation
+
+Pulser is a framework for composing, simulating and executing **pulse** sequences for neutral-atom quantum devices.
+
+This is the `pulser-simulation` extension, which provides the functionalities needed to emulate `pulser` sequences.
+
+## Installation
+
+The standard Pulser installation,
+
+```bash
+pip install pulser
+```
+
+will automatically install `pulser-simulation`. If you wish to install it on its own, you can also run
+
+```bash
+pip install pulser-simulation
+```
+
+Note that `pulser-core` is a requirement of `pulser-simulation`, so it will be installed if it hasn't been already.
---
+++ pulser-simulation/setup.py
@@ -0,0 +1,50 @@
+# Copyright 2022 Pulser Development Team
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from setuptools import find_packages, setup
+
+__version__ = ""
+exec(open("pulser_simulation/_version.py").read())
+
+setup(
+    name="pulser_simulation",
+    version=__version__,
+    install_requires=[
+        "matplotlib",
+        "numpy>=1.20",
+        "scipy",
+        "qutip>=4.6.3",
+        "pulser>0.5.0",
+    ],
+    extras_require={
+        ":python_version == '3.7'": [
+            "typing-extensions",
+        ],
+    },
+    packages=find_packages(),
+    package_data={"pulser": ["py.typed"]},
+    include_package_data=True,
+    description="An emulator of pulse-level sequences created with Pulser.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Pulser Development Team",
+    python_requires=">=3.7.0",
+    license="Apache 2.0",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+    ],
+    url="https://github.com/pasqal-io/Pulser",
+    zip_safe=False,
+)
