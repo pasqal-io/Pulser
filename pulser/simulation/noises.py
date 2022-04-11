702af20fb5a4634e9add5ee2c97bd08889f28196
Henrique Silvério <henrique.silverio@tecnico.ulisboa.pt>
2022-04-11 15:13:22 +0200

    Updates to `pulser.sampler` (#360)
    * Separating specific noise definitions from general noise objects
    
    * Adding missing licenses

--- pulser/simulation/noises.py
+++ pulser/simulation/noises.py
@@ -1,3 +1,16 @@
+# Copyright 2022 Pulser Development Team
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """Contains noise models.
 
 For now, only the amplitude and doppler noises are implemented in the form a
@@ -6,7 +19,6 @@ sampling time.
 """
 from __future__ import annotations
 
-import functools
 from typing import Callable, Optional
 
 import numpy as np
@@ -104,39 +116,3 @@ def doppler(reg: Register, std_dev: float, seed: Optional[int]) -> NoiseModel:
         )
 
     return f
-
-
-def compose_local_noises(*functions: NoiseModel) -> NoiseModel:
-    """Helper to compose multiple NoiseModel.
-
-    Args:
-        *functions: a list of functions
-
-    Returns:
-        The mathematical composition of *functions. The last element is applied
-        first. If *functions is [f, g, h], it returns f∘g∘h.
-    """
-    return functools.reduce(
-        lambda f, g: lambda x: f(g(x)), functions, lambda x: x
-    )
-
-
-def apply(
-    samples: list[QubitSamples], noises: list[NoiseModel]
-) -> list[QubitSamples]:
-    """Apply a list of NoiseModel on a list of QubitSamples.
-
-    The noises are composed using the compose_local_noises function, such that
-    the last element is applied first.
-
-    Args:
-        samples (list[QubitSamples]): A list of QubitSamples.
-        noises (list[NoiseModel]): A list of NoiseModel.
-
-    Return:
-        A list of QubitSamples on which each element of noises has been
-        applied.
-    """
-    tot_noise = compose_local_noises(*noises)
-
-    return [tot_noise(s) for s in samples]
