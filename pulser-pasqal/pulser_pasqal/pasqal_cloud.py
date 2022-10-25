75fcf8b1ca937022982241a41649417644948274
CdeTerra <102144942+CdeTerra@users.noreply.github.com>
2022-10-25 11:36:13 +0200

    Add PasqalCloud class and UT

---
+++ pulser-pasqal/pulser_pasqal/pasqal_cloud.py
@@ -0,0 +1,93 @@
+# Copyright 2022 Pulser Development Team
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Allows to connect to the cloud powered by Pasqal to run sequences."""
+from typing import Dict, List, Mapping, Optional, Union
+
+import sdk
+from numpy.typing import ArrayLike
+
+from pulser import Sequence
+from pulser.register import QubitId
+
+
+class PasqalCloud:
+    """Manager of the connection to the cloud powered by Pasqal.
+
+    The cloud connection enables to run sequences on simulators or on real
+    QPUs.
+    """
+
+    def __init__(
+        self, client_id, client_secret, endpoints: sdk.endpoints.Endpoints
+    ):
+        """Initializes a connection to the cloud.
+
+        Args:
+            client_id: client_id of the API key you are holding for Pasqal
+                cloud.
+            client_secret: client_secret of the API key you are holding for
+                Pasqal cloud.
+            endpoints: Optionally, Pasqal cloud connection URLs.
+        """
+        self._sdk_connection = sdk.SDK(
+            client_id=client_id,
+            client_secret=client_secret,
+            endpoints=endpoints,
+        )
+
+    def create_batch(
+        self,
+        seq: Sequence,
+        jobs: List[Dict[str, Union[ArrayLike, Mapping[QubitId, int]]]],
+        device_type: sdk.DeviceType = sdk.DeviceType.QPU,
+        configuration: Optional[sdk.Configuration] = None,
+        wait: bool = False,
+    ) -> sdk.Batch:
+        """Create a new batch and send it to the API.
+
+        For Iroise MVP, the batch must contain at least one job and will be
+        declared as complete immediately.
+
+        Args:
+            seq: Pulser sequence.
+            jobs: List of jobs to be added to the batch at creation.
+            device_type: The type of device to use, either an emulator or a QPU
+                If set to QPU, the device_type will be set to the one
+                stored in the serialized sequence.
+            configuration: A dictionary with extra configuration for the
+                emulators that accept it.
+            wait: Whether to wait for results to be sent back.
+
+        Returns:
+            Batch: The new batch that has been created in the database.
+        """
+        self._sdk_connection.create_batch(
+            serialized_sequence=seq.serialize(),
+            jobs=jobs,
+            device_type=device_type,
+            configuration=configuration,
+            wait=wait,
+        )
+
+    def get_batch(self, id: int, fetch_results: bool = False) -> sdk.Batch:
+        """Retrieve a batch's data and all its jobs.
+
+        Args:
+            id: Id of the batch.
+            fetch_results: Whether to load job results.
+
+        Returns:
+            Batch: The batch stored in the PCS database.
+        """
+        self._sdk_connection.get_batch(id=id, fetch_results=fetch_results)
--- tests/test_pasqal.py
+++ tests/test_pasqal.py
@@ -14,12 +14,66 @@
 from __future__ import annotations
 
 from pathlib import Path
+from unittest.mock import patch
 
 import pulser
 import pulser_pasqal
+from pulser.devices import Chadoq2
+from pulser.register import Register
+from pulser.sequence import Sequence
+from pulser_pasqal import Configuration, DeviceType, Endpoints, PasqalCloud
 
 root = Path(__file__).parent.parent
 
 
 def test_version():
     assert pulser_pasqal.__version__ == pulser.__version__
+
+
+def test_pasqal_cloud():
+    with patch("sdk.SDK", autospec=True) as mock_cloud_sdk_class:
+        pasqal_cloud_kwargs = dict(
+            client_id="abc",
+            client_secret="def",
+            endpoints=Endpoints(core="core_url", account="account_url"),
+        )
+
+        pasqal_cloud = PasqalCloud(**pasqal_cloud_kwargs)
+
+        mock_cloud_sdk_class.assert_called_once_with(**pasqal_cloud_kwargs)
+
+        mock_cloud_sdk = mock_cloud_sdk_class.return_value
+
+        reg = Register(dict(enumerate([(0, 0), (0, 10)])))
+        device = Chadoq2
+        seq = Sequence(reg, device)
+
+        create_batch_kwargs = dict(
+            jobs=[{"runs": 10, "variables": {"a": [3, 5]}}],
+            device_type=DeviceType.QPU,
+            configuration=Configuration(
+                dt=0.1,
+                precision="normal",
+                extra_config=None,
+            ),
+            wait=True,
+        )
+
+        pasqal_cloud.create_batch(
+            seq,
+            **create_batch_kwargs,
+        )
+
+        mock_cloud_sdk.create_batch.assert_called_once_with(
+            seq.serialize(),
+            **create_batch_kwargs,
+        )
+
+        get_batch_kwargs = dict(
+            id=10,
+            fetch_results=True,
+        )
+
+        pasqal_cloud.get_batch(**get_batch_kwargs)
+
+        mock_cloud_sdk.get_batch.assert_called_once_with(**get_batch_kwargs)
