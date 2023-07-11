031523ba2180973facacd236650d042a55f82d0f
Antoine Cornillot <61453516+a-corni@users.noreply.github.com>
2023-07-11 16:02:33 +0200

    Validate device, seq against the abstract representation schema in serialization (#550)
    * Adding validation of device, seq in serialization
    
    * sorting imports
    
    * Introducing init file with variables
    
    * Taking into account review comments

--- pulser-core/pulser/devices/_device_datacls.py
+++ pulser-core/pulser/devices/_device_datacls.py
@@ -26,6 +26,7 @@ from scipy.spatial.distance import pdist, squareform
 from pulser.channels.base_channel import Channel
 from pulser.devices.interaction_coefficients import c6_dict
 from pulser.json.abstract_repr.serializer import AbstractReprEncoder
+from pulser.json.abstract_repr.validation import validate_abstract_repr
 from pulser.json.utils import obj_to_dict
 from pulser.register.base_register import BaseRegister, QubitId
 from pulser.register.mappable_reg import MappableRegister
@@ -417,7 +418,9 @@ class BaseDevice(ABC):
 
     def to_abstract_repr(self) -> str:
         """Serializes the Sequence into an abstract JSON object."""
-        return json.dumps(self, cls=AbstractReprEncoder)
+        abstr_dev_str = json.dumps(self, cls=AbstractReprEncoder)
+        validate_abstract_repr(abstr_dev_str, "device")
+        return abstr_dev_str
 
 
 @dataclass(frozen=True, repr=False)
