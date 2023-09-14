c4693ed2da88267e4961c11cb6f981ba82ebe607
Antoine Cornillot <61453516+a-corni@users.noreply.github.com>
2023-09-14 18:16:54 +0200

    Bug Fix: error message in switch_device (#580)
    * Fixing error message in switch_device
    
    * Modifying comments

--- pulser-core/pulser/sequence/sequence.py
+++ pulser-core/pulser/sequence/sequence.py
@@ -553,6 +553,9 @@ class Sequence(Generic[DeviceType]):
                         continue
                 if not strict:
                     channel_match[old_ch_name] = new_ch_id
+                    # Found a match, clear match error msg for this channel
+                    if ch_match_err.startswith(base_msg):
+                        ch_match_err = ""
                     break
 
                 params_to_check = [
@@ -574,6 +577,11 @@ class Sequence(Generic[DeviceType]):
                 else:
                     # Only reached if all checks passed
                     channel_match[old_ch_name] = new_ch_id
+                    # Found a match, clear match error msgs for this channel
+                    if ch_match_err.startswith(base_msg):
+                        ch_match_err = ""
+                    if strict_error_message.startswith(base_msg):
+                        strict_error_message = ""
                     break
 
         if None in channel_match.values():
