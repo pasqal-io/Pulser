c58ada8520518357672a65d8c3897c83e64ee3e7
HGSilveri <henrique.silverio@tecnico.ulisboa.pt>
2024-01-11 11:20:33 +0100

    Fix typo

--- pulser-simulation/pulser_simulation/__init__.py
+++ pulser-simulation/pulser_simulation/__init__.py
@@ -15,7 +15,7 @@
 
 from pulser.backend import EmulatorConfig, NoiseModel
 
-from pulser_simulation._version import __version__ as __version
+from pulser_simulation._version import __version__ as __version__
 from pulser_simulation.qutip_backend import QutipBackend
 from pulser_simulation.simconfig import SimConfig
 from pulser_simulation.simulation import QutipEmulator, Simulation
