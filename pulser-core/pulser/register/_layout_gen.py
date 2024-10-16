5be783d724902967e19ac06490181510420523c5
HGSilveri <henrique.silverio@tecnico.ulisboa.pt>
2024-10-16 10:09:15 +0200

    Adding the register layout generator

---
+++ pulser-core/pulser/register/_layout_gen.py
@@ -0,0 +1,90 @@
+# Copyright 2024 Pulser Development Team
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from __future__ import annotations
+
+import numpy as np
+from scipy.spatial.distance import cdist
+
+from pulser.register import Register, RegisterLayout
+
+
+def generate_register_layout(
+    register: Register,
+    min_trap_dist: float,
+    max_radial_dist: int,
+    max_layout_filling: float,
+    optimal_layout_filling: float | None = None,
+    mesh_resolution: float = 1.0,
+    slug: str | None = None,
+) -> RegisterLayout:
+    """Generates a layout for a register.
+
+    Args:
+        register: The register to generate the layout for.
+        device: The Pulser device on which the layout and register
+            will be implemented.
+        min_trap_dist: The minimum distance between traps.
+        max_radial_dist: The maximum distance from the origin.
+        max_layout_filling: The maximum ratio of atoms to traps.
+        optimal_layout_filling: An optional value for the optimal ratio of
+            atoms to traps. If not given, takes max_layout_filling.
+        mesh_resolution: The spacing between points in the mesh of candidate
+            coordinates, in µm.
+        slug: An optional slug for the resulting layout.
+
+    Returns:
+        RegisterLayout: The register layout.
+    """
+    optimal_layout_filling = optimal_layout_filling or max_layout_filling
+
+    # Generate all coordinates where a trap can be placed
+    lx = 2 * max_radial_dist
+    side = np.linspace(0, lx, num=int(lx / mesh_resolution)) - max_radial_dist
+    x, y = np.meshgrid(side, side)
+    in_circle = x**2 + y**2 <= max_radial_dist**2
+    coords = np.c_[x[in_circle].ravel(), y[in_circle].ravel()]
+
+    # Get the atoms in the register (the "seeds")
+    seeds = list(register.qubits.values())
+    N_seeds = len(seeds)
+
+    # Record indices and distances between coords and seeds
+    c_indx = np.arange(len(coords))
+    all_dists = cdist(coords, seeds)
+
+    total_extra_traps = (
+        np.ceil(N_seeds / optimal_layout_filling).astype(int) - N_seeds
+    )
+    min_extra_traps = (
+        np.ceil(N_seeds / max_layout_filling).astype(int) - N_seeds
+    )
+    # This is the region where we can still add traps
+    region_left = np.all(all_dists > min_trap_dist, axis=1)
+    # The traps start out as being just the seeds
+    traps = seeds.copy()
+    for i in range(total_extra_traps):
+        if not np.any(region_left) and i + 1 < min_extra_traps:
+            raise RuntimeError(
+                f"Failed to find a site for {min_extra_traps - i} traps."
+            )
+        # Select the point in the valid region that is closest to a seed
+        selected = c_indx[region_left][
+            np.argmin(np.min(all_dists[region_left][:, :N_seeds], axis=1))
+        ]
+        # Add the selected point to the traps
+        traps.append(coords[selected])
+        # Add the distances to the new trap
+        all_dists = np.append(all_dists, cdist(coords, [traps[-1]]), axis=1)
+        region_left *= all_dists[:, -1] > min_trap_dist
+    return RegisterLayout(traps, slug=slug)
