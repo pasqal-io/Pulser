4008e18c8b77d60d97cd58e0cae2540162aacce1
Vytautas Abramavicius <vytautas.abramavicius@pasqal.com>
2025-02-04 17:48:48 +0200

    fixed test

--- tests/test_parametrized.py
+++ tests/test_parametrized.py
@@ -236,7 +236,8 @@ def test_opsupport(a, b, with_diff_tensor):
     y = np.tanh(b)
     check_var_grad(y)
     np.testing.assert_almost_equal(
-        y.build().as_array(detach=with_diff_tensor), np.tanh(b.build().as_array(detach=with_diff_tensor))
+        y.build().as_array(detach=with_diff_tensor),
+        np.tanh(b.build().as_array(detach=with_diff_tensor)),
     )
     y = np.exp(b)
     check_var_grad(y)
