b18fb3ae4c2aff1d6f276b185dbca011b735b9bf
Kemal Bidzhiev <kemal@Kemals-MacBook-Pro.local>
2025-08-27 21:27:45 +0200

    abstract repr

--- tests/test_abstract_repr.py
+++ tests/test_abstract_repr.py
@@ -2789,8 +2789,16 @@ class TestDeserialization:
             Sequence.from_abstract_repr(s)
 
 
-@pytest.mark.parametrize("detuning_sigma", [0.0, 1.0])
-def test_noise_optional_params(detuning_sigma):
+@pytest.mark.parametrize(
+    "detuning_sigma, detuning_hf_psd, detuning_hf_freqs",
+    [
+        (0.0, (), ()),
+        (1.0, (1.0, 2.0, 3.0), (4.0, 5.0, 6.0)),
+    ],
+)
+def test_noise_optional_params(
+    detuning_sigma, detuning_hf_psd, detuning_hf_freqs
+):
     noise = pulser.noise_model.NoiseModel(
         runs=1,  # TODO: connect this with MCArlo
         samples_per_run=1,  # TODO: connect this with MCarlo or ignored
@@ -2800,6 +2808,8 @@ def test_noise_optional_params(detuning_sigma):
         laser_waist=5,
         amp_sigma=0.1,
         detuning_sigma=detuning_sigma,
+        detuning_hf_psd=detuning_hf_psd,
+        detuning_hf_freqs=detuning_hf_freqs,
         temperature=10.0,
         with_leakage=True,
         eff_noise_rates=(0.1,),
@@ -2808,3 +2818,5 @@ def test_noise_optional_params(detuning_sigma):
     )
     repr = noise._to_abstract_repr()
     assert ("detuning_sigma" in repr) == (detuning_sigma != 0.0)
+    assert ("detuning_hf_psd" in repr) == (detuning_hf_psd != ())
+    assert ("detuning_hf_freqs" in repr) == (detuning_hf_freqs != ())
