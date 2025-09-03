5941002f2cb20290200dd239eb623f7c5904b5ce
murogrande <murogrande@yahoo.com>
2025-09-03 16:06:27 +0200

    doc

--- pulser-core/pulser/noise_model.py
+++ pulser-core/pulser/noise_model.py
@@ -141,13 +141,16 @@ class NoiseModel:
       Parametrized by the ``temperature`` field.
     - **register**: Thermal fluctuations in the
       register positions, parametrized by ``temperature``, ``trap_waist``
-      and, ``trap_depth``, which must all be defined,
-      - Plane standard deviation:
-      :math:`\sigma^{xy} = \sqrt{\frac{T w²}{4 U_{trap}}}`, where T is
-      temperature, w is the trap waist and :math:`U_{trap}` is the trap depth.
-      - Off plane standard deviation:
-      :math:`\sigma^z = \frac{\pi}{\lambda}\sqrt{2} w \sigma^{xy}`, where
-      :math:`\lambda` is the trap wavelength with a constant value of 0.85 µm.
+      and, ``trap_depth``, which must all be defined
+
+        - Plane standard deviation:
+        :math:`\sigma^{xy} = \sqrt{\frac{T w²}{4 U_{trap}}}`, where T is
+        temperature, w is the trap waist and :math:`U_{trap}` is
+        the trap depth.
+        - Off plane standard deviation:
+        :math:`\sigma^z = \frac{\pi}{\lambda}\sqrt{2} w \sigma^{xy}`, where
+        :math:`\lambda` is the trap wavelength with a constant value of
+        0.85 µm.
     - **amplitude**: Gaussian damping due to finite laser waist and
       laser amplitude fluctuations. Parametrized by ``laser_waist``
       and ``amp_sigma``.
