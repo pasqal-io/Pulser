96da65df7d3c5308c8f1c43cc5cac800ecdb7364
murogrande <murogrande@yahoo.com>
2025-09-03 15:00:39 +0200

    documentation again

--- pulser-core/pulser/noise_model.py
+++ pulser-core/pulser/noise_model.py
@@ -142,7 +142,6 @@ class NoiseModel:
     - **register**: Thermal fluctuations in the
       register positions, parametrized by ``temperature``, ``trap_waist``
       and, ``trap_depth``, which must all be defined,
-
       - Plane standard deviation:
       :math:`\sigma^{xy} = \sqrt{\frac{T w²}{4 U_{trap}}}`, where T is
       temperature, w is the trap waist and :math:`U_{trap}` is the trap depth.
