ccd7f1eb61be39bfbc07729f0a8e2c5842d4e5a7
Henrique Silvério <29920212+HGSilveri@users.noreply.github.com>
2026-02-03 10:45:06 +0100

    Create `BackendConfig.__repr__()` (#1008)
    * Create `BackendConfig.__repr__()`
    
    * Fix Python 3.10 compat

--- pulser-core/pulser/backend/config.py
+++ pulser-core/pulser/backend/config.py
@@ -137,6 +137,12 @@ class BackendConfig:
             "desired changes."
         )
 
+    def __repr__(self) -> str:
+        params_str = ",\n    ".join(
+            f"{key}={value!r}" for key, value in self._backend_options.items()
+        )
+        return f"{self.__class__.__name__}(\n    {params_str},\n)"
+
 
 class EmulationConfig(BackendConfig, Generic[StateType]):
     """Configures an emulation on a backend.
