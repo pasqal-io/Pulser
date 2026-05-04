78a1bea1ec1f0df9f15f9047a0ac29762acfedd1
stephenprn <stephen.perrin@2017.icam.fr>
2026-05-04 10:08:25 +0200

    poc: new fastjsonschema validation lib

--- pulser-core/pulser/json/utils.py
+++ pulser-core/pulser/json/utils.py
@@ -18,7 +18,7 @@ from __future__ import annotations
 import json
 import warnings
 from dataclasses import MISSING, Field
-from typing import TYPE_CHECKING, Any, Optional, Sequence
+from typing import TYPE_CHECKING, Any, Literal, Optional, Sequence
 
 import numpy as np
 
@@ -116,3 +116,18 @@ def stringify_qubit_ids(qubit_ids: Sequence[QubitId]) -> list[str]:
                 f"strings for IDs: {[(id, str(id)) for id in collisions]}"
             )
     return names
+
+
+ObjectType = Literal[
+    "sequence",
+    "device",
+    "layout",
+    "register",
+    "noise",
+    "results",
+    "config",
+]
+
+
+def get_filename(object_type: ObjectType) -> str:
+    return f"{object_type}-schema.json"
