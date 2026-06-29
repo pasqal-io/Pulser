8403e5252b0196c6656c9373d5c34b99f791dfa9
Alexandre Papin <alexandre.papin@pasqal.com>
2026-06-29 15:59:56 +0200

    tmp: update pasqal-cloud to latest commit

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ if "dev" in __version__:
 
 # Pulser packages not pinned to __version__
 requirements = [
-    "pasqal-cloud @ git+https://github.com/pasqal-io/pasqal-cloud@ap/1822-rename-classes#egg=pasqal-cloud&subdirectory=pasqal-cloud",
+    "pasqal-cloud @ git+https://github.com/pasqal-io/pasqal-cloud@af6b4f8ba11ed502f0d7b27d681370c8a9feb3f3#egg=pasqal-cloud&subdirectory=pasqal-cloud",
 ]
 # Adding packages pinned to __version__
 with open("packages.txt", "r", encoding="utf-8") as f:
